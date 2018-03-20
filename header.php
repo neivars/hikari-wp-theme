695cf0fe76853ddd82e1ae1440463584684745a6
Raul Neiva <raulsneiva@gmail.com>
2018-03-20 00:49:27 +0000

    Plugin hook tags added

--- header.php
+++ header.php
@@ -14,5 +14,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title><?php bloginfo( 'name' ); ?></title>
+
+	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
